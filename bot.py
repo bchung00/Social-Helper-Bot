2315cd624b5de7d39779e3bfc7edb587dd6ffe56
Brandon Chung <b.chung8612@gmail.com>
2018-12-01 13:10:02 -0800

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from slackclient import SlackClient
 
-slack_token = open("bot-token.txt","r").read()
+slack_token = 'xoxb-493304761941-493309119797-HKQEeEajSVSQjpg5kAApFOOg'
 sc = SlackClient(slack_token)
 
 sc.api_call(
