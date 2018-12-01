f1706102c592d8bddc265cac3538d2e300532aa2
Brandon Chung <b.chung8612@gmail.com>
2018-12-01 13:31:18 -0800

    Added real time message reading for bot

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 from slackclient import SlackClient
+import time
 
-slack_token = 'xoxb-493304761941-493309119797-HKQEeEajSVSQjpg5kAApFOOg'
+slack_token = 'xoxb-493304761941-494539529958-xxyoXNq678MuK50kPSh2EoAE'
 sc = SlackClient(slack_token)
+
+if sc.rtm_connect():
+  while sc.server.connected is True:
+        print (sc.rtm_read())
+        time.sleep(1)
+else:
+    print ("Connection Failed")
 
 sc.api_call(
   "chat.postMessage",
