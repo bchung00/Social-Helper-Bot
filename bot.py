59638a899ffde066146a6bb3111b66a0cff32159
Brandon Chung <b.chung8612@gmail.com>
2018-12-01 14:27:02 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 from slackclient import SlackClient
 import time
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
 slack_token = 'xoxb-493304761941-494539529958-xxyoXNq678MuK50kPSh2EoAE'
 sc = SlackClient(slack_token)
 
 if sc.rtm_connect():
-  while sc.server.connected is True:
-        print (sc.rtm_read())
-        time.sleep(1)
+  while True:
+    ob = sc.rtm_read()
+    print(ob)
+    i = 0
+    for x in ob:
+        if ('type' in x and x['type'] == 'message'):
+            score = analyzer.polarity_scores(x['text'])
+            sc.rtm_send_message("general", "test")
+    time.sleep(3)
 else:
     print ("Connection Failed")
 
