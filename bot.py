bf9284bad7e1a990572f63f4c737ed04459526f6
Brandon Chung <b.chung8612@gmail.com>
2018-12-01 12:54:09 -0800

    added test message

--- bot.py
+++ bot.py
@@ -2,3 +2,9 @@ from slackclient import SlackClient
 
 slack_token = open("bot-token.txt","r").read()
 sc = SlackClient(slack_token)
+
+sc.api_call(
+  "chat.postMessage",
+  channel="C0XXXXXX",
+  text="Hello from Python! :tada:"
+)
