69062c84788a10fe602b977344a968b64dde2dba
Brandon Chung <b.chung8612@gmail.com>
2018-12-01 14:51:32 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,15 +9,17 @@ slack_token = 'xoxb-493304761941-494539529958-xxyoXNq678MuK50kPSh2EoAE'
 sc = SlackClient(slack_token)
 
 if sc.rtm_connect():
+    print('success')
+if sc.rtm_connect():
   while True:
     ob = sc.rtm_read()
     print(ob)
     i = 0
     for x in ob:
         if ('type' in x and x['type'] == 'message'):
-            score = analyzer.polarity_scores(x['text'])
-            sc.rtm_send_message("general", "test")
-    time.sleep(3)
+            score = analyzer.polarity_scores(x['text']).catch(Exception)
+            sc.rtm_send_message("general", "Postivity score: " + str(score['compound']*100) + "%")
+    time.sleep(1)
 else:
     print ("Connection Failed")
 
